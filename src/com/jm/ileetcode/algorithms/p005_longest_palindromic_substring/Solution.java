c0e65c42c13d888e3a28ff5f9af44c8df9c6f11f
Johnny Miller <johnnysviva@outlook.com>
2018-05-08 21:57:16 +0800

    Clean code

--- src/com/jm/ileetcode/algorithms/p005_longest_palindromic_substring/Solution.java
+++ src/com/jm/ileetcode/algorithms/p005_longest_palindromic_substring/Solution.java
@@ -1,19 +1,23 @@
 package com.jm.ileetcode.algorithms.p005_longest_palindromic_substring;
 
 public class Solution {
+    public String longestPalindrome(String s) {
+        return null;
+    }
 
+    public static void main(String[] args) {
+
+    }
 }
 /**
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
- *
+ * <p>
  * Example 1:
- *
  * Input: "babad"
  * Output: "bab"
  * Note: "aba" is also a valid answer.
- *
+ * <p>
  * Example 2:
- *
  * Input: "cbbd"
  * Output: "bb"
  */
